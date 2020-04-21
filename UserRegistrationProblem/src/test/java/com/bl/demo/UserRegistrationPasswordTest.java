198da67a7a9fa785c59ace5aa0a754247665dd29
250399 <kunaldeshmukh2503@gmail.com>
2020-04-21 15:25:06 -0700

    MODIFIED: Password should contain atleast one Uppercase alphabet

--- UserRegistrationProblem/src/test/java/com/bl/demo/UserRegistrationPasswordTest.java
+++ UserRegistrationProblem/src/test/java/com/bl/demo/UserRegistrationPasswordTest.java
@@ -8,12 +8,14 @@ import static org.junit.Assert.*;
 
 public class UserRegistrationPasswordTest {
     String passwordRule1;
+    String passwordRule2;
     UserRegistration obj;
 
     @Before
     public void init(){
         obj=new UserRegistration();
         passwordRule1="[a-zA-Z0-9]{8,}";
+        passwordRule2="(?=.*[A-Z])[a-zA-Z0-9]{8,}";
     }
 
     @Test
@@ -24,5 +26,14 @@ public class UserRegistrationPasswordTest {
     @Test
     public void testPasses_ifInput_DoesNotMatchesPasswordRule1(){
         Assert.assertFalse(obj.validate(passwordRule1,"asdf12"));
+    }
+    @Test
+    public void testPasses_ifInput_MatchesPasswordRule2(){
+        Assert.assertTrue(obj.validate(passwordRule2,"asdfA12345"));
+    }
+
+    @Test
+    public void testPasses_ifInput_DoesNotMatchesPasswordRule2(){
+        Assert.assertFalse(obj.validate(passwordRule2,"asdf12sadsad"));
     }
 }

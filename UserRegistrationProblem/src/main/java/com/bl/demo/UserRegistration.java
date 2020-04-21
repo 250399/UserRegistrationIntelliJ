7d6a732921aa0120d911c96478668986ed6fce69
250399 <kunaldeshmukh2503@gmail.com>
2020-04-21 12:37:41 -0700

    ADDED: First name starts with Capital and has atleast 3 characters

--- UserRegistrationProblem/src/main/java/com/bl/demo/UserRegistration.java
+++ UserRegistrationProblem/src/main/java/com/bl/demo/UserRegistration.java
@@ -1,5 +1,9 @@
 package com.bl.demo;
 
-public class UserRegistration {
+import java.util.regex.Pattern;
 
+public class UserRegistration {
+    public boolean validate(String patternString,String data){
+        return Pattern.matches(patternString,data);
+    }
 }
--- UserRegistrationProblem/src/test/java/com/bl/demo/UserRegistrationTest.java
+++ UserRegistrationProblem/src/test/java/com/bl/demo/UserRegistrationTest.java
@@ -1,7 +1,25 @@
 package com.bl.demo;
 
-import static org.junit.Assert.*;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+import static org.junit.Assert.*;
 public class UserRegistrationTest {
+    UserRegistration obj = new UserRegistration();
+    String firstNamePattern="^[A-Z][a-z]{2,}";
+
+
+    @Test
+    public void testPasses_ForFirstName_WhenInputisValid(){
+        Assert.assertTrue(obj.validate(firstNamePattern,"Kunal"));
+    }
+
+    @Test
+    public void testPasses_ForFirstName_WhenInputisInvalid(){
+        Assert.assertFalse(obj.validate(firstNamePattern,"kunal"));
+    }
 
 }

cfa5491bd9f18f48c5b73d38f146a2e529e5f897
250399 <kunaldeshmukh2503@gmail.com>
2020-04-21 15:43:35 -0700

    MODIFIED: Password must also include exact one special character

--- UserRegistrationProblem/src/test/java/com/bl/demo/UserRegistrationPasswordTest.java
+++ UserRegistrationProblem/src/test/java/com/bl/demo/UserRegistrationPasswordTest.java
@@ -3,14 +3,25 @@ package com.bl.demo;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 
 import static org.junit.Assert.*;
 
+@RunWith(Parameterized.class)
 public class UserRegistrationPasswordTest {
     String passwordRule1;
     String passwordRule2;
     String passwordRule3;
+    String passwordRule4;
+    String input;
+    boolean expectedValue;
     UserRegistration obj;
+
 
     @Before
     public void init(){
@@ -18,6 +29,22 @@ public class UserRegistrationPasswordTest {
         passwordRule1="[a-zA-Z0-9]{8,}";
         passwordRule2="(?=.*[A-Z])[a-zA-Z0-9]{8,}";
         passwordRule3="(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}";
+        passwordRule4="^(?=.*[0-9])(?=.*[A-Z])(?=[a-zA-Z0-9]*[^a-zA-Z0-9][a-zA-Z0-9]*$).{8,}";
+    }
+
+
+    public UserRegistrationPasswordTest(String input, boolean expectedValue){
+        this.input=input;
+        this.expectedValue=expectedValue;
+    }
+
+    @Parameterized.Parameters
+    public static Collection checkPassword(){
+        return Arrays.asList(new Object[][]{
+                {"@Kaneki322",true},
+                {"@Kanek@322",false},
+                {"Kaneki322",false}
+        });
     }
 
     @Test
@@ -47,4 +74,9 @@ public class UserRegistrationPasswordTest {
     public void testPasses_ifInput_DoesNotMatchesPasswordRule3(){
         Assert.assertFalse(obj.validate(passwordRule3,"asdfsadsad"));
     }
+
+    public void validatePassword() {
+    Assert.assertEquals(expectedValue,obj.validate(input,passwordRule4));
+    }
+
 }

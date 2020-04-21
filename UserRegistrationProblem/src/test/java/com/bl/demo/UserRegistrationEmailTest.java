1d38499596ab6fc38d3dbac26944f92cd077e386
250399 <kunaldeshmukh2503@gmail.com>
2020-04-21 14:54:59 -0700

    ADDED: Email should be in valid format satisfying all given conditions

---
+++ UserRegistrationProblem/src/test/java/com/bl/demo/UserRegistrationEmailTest.java
@@ -0,0 +1,66 @@
+package com.bl.demo;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.lang.reflect.Array;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+@RunWith(Parameterized.class)
+public class UserRegistrationEmailTest {
+    boolean expectedValue;
+    String input;
+    String inputPattern;
+    UserRegistration obj;
+
+    public UserRegistrationEmailTest(String input,boolean expectedValue){
+        this.input=input;
+        this.expectedValue=expectedValue;
+    }
+
+    @Before
+    public void initializeObject(){
+        obj=new UserRegistration();
+        inputPattern="^[a-zA-Z0-9]+[\\.\\-\\+\\_]?[a-zA-Z0-9]+@[a-zA-Z0-9]+[.]?[a-zA-Z]{2,4}[\\.]?([a-z]{2,4})?$";
+    }
+
+    @Parameterized.Parameters
+    public static Collection checkEmail(){
+        return Arrays.asList(new Object[][] {{"abc@yahoo.com",true},
+                {"abc-100@yahoo.com",true},
+                {"abc.100@yahoo.com",true},
+                {"abc111@abc.com",true},
+                {"abc-100@abc.net",true},
+                {"abc.100@abc.com.au",true},
+                {"abc@1.com",true},
+                {"abc@gmail.com.com",true},
+                {"abc+100@gmail.com",true},
+                {"abc",false},
+                {"abc@.com.my",false},
+                {"abc123@gmail.a",false},
+                {"abc123@.com",false},
+                {"abc123@.com.com",false},
+                {".abc@abc.com",false},
+                {"abc()*@gmail.com",false},
+                {"abc@%*.com",false},
+                {"abc..2002@gmail.com",false},
+                {"abc.@gmail.com",false},
+                {"abc@abc@gmail.com",false},
+                {"abc@gmail.com.1a",false},
+                {"abc@gmail.com.aa.au",false}
+        });
+    }
+
+    @Test
+    public void validateEmail(){
+        Assert.assertEquals(expectedValue,obj.validate(inputPattern,input));
+    }
+
+}
--- UserRegistrationProblem/src/test/java/com/bl/demo/UserRegistrationTest.java
+++ UserRegistrationProblem/src/test/java/com/bl/demo/UserRegistrationTest.java
@@ -6,7 +6,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
+
 public class UserRegistrationTest {
     UserRegistration obj = new UserRegistration();
     String firstNamePattern="^[A-Z][a-z]{2,}";
